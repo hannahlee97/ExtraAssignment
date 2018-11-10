c9caa3f9b20649baf002806ecf3641fe7aebc5ef
Haeun Lee(Hannah) <hlee405@my.bcit.ca>
2018-11-10 14:58:32 -0800

    final commit

---
+++ cmake-build-debug/CMakeFiles/ExtraAssignment.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/hannahlee97/CLionProjects/ExtraAssignment/Bicycle.cpp" "/Users/hannahlee97/CLionProjects/ExtraAssignment/cmake-build-debug/CMakeFiles/ExtraAssignment.dir/Bicycle.cpp.o"
+  "/Users/hannahlee97/CLionProjects/ExtraAssignment/Car.cpp" "/Users/hannahlee97/CLionProjects/ExtraAssignment/cmake-build-debug/CMakeFiles/ExtraAssignment.dir/Car.cpp.o"
+  "/Users/hannahlee97/CLionProjects/ExtraAssignment/DerivedVehicle.cpp" "/Users/hannahlee97/CLionProjects/ExtraAssignment/cmake-build-debug/CMakeFiles/ExtraAssignment.dir/DerivedVehicle.cpp.o"
+  "/Users/hannahlee97/CLionProjects/ExtraAssignment/main.cpp" "/Users/hannahlee97/CLionProjects/ExtraAssignment/cmake-build-debug/CMakeFiles/ExtraAssignment.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/ExtraAssignment.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ExtraAssignment.dir/main.cpp.o"
+  "CMakeFiles/ExtraAssignment.dir/DerivedVehicle.cpp.o"
+  "CMakeFiles/ExtraAssignment.dir/Car.cpp.o"
+  "CMakeFiles/ExtraAssignment.dir/Bicycle.cpp.o"
+  "ExtraAssignment.pdb"
+  "ExtraAssignment"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ExtraAssignment.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
